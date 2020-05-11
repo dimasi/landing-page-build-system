115cef6d884a51c3f849053c16def9e4d73f9211
dimasi <d.sivachenko@hotmail.com>
2020-05-11 14:38:45 +0300

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 // Vendor
-// import 'modernizr';
 import svg4everybody from 'svg4everybody';
-
-// Polyfills
-// import 'babel-polyfill';
 
 // Components
 import '@/components/button';
@@ -16,16 +12,4 @@ import './style.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
     svg4everybody();
-});
-
-function testPromise() {
-    return new Promise(function(resolve) {
-        setTimeout(function() {
-            resolve();
-        }, 3000);
-    });
-}
-
-testPromise().then(function() {
-    console.log('Promise resolve');
 });
